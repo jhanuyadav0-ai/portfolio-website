16f78f1d4368281acff7217f58318a63ffbddd49
jhanuyadav0-ai <jhanuyadav0@gmail.com>
2026-06-19 23:15:17 +0530

    Professional Portfolio Website

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Portfolio Website Loaded");
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('scroll',()=>{
+
+cards.forEach(card=>{
+
+const position = card.getBoundingClientRect().top;
+
+const screen = window.innerHeight;
+
+if(position < screen-100){
+
+card.style.opacity="1";
+card.style.transform="translateY(0px)";
+
+}
+
+});
+
+});
